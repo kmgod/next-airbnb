88488428be842f0e5bb1cb4d3bf6e42ccc81b83e
androol <multiad05@gmail.com>
2023-10-02 18:17:38 +0900

    document update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,12 @@
-import Document, { DocumentContext } from "next/document";
+import Document, { 
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const orginalRenderPage = ctx.renderPage;
@@ -27,4 +32,25 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+  render() {
+    return (
+        <Html>
+            <Head>
+                <link
+                href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap"
+                rel="stylesheet"
+                />
+                <link
+                href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean"
+                rel="stylesheet"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+  }
 }
+export default MyDocument;
