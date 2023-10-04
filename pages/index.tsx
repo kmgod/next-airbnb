6a189dea6da6b47cddadf9870c6fbf8da1152c7a
androol <multiad05@gmail.com>
2023-10-04 17:16:49 +0900

    SearchRoomBar index.tsx added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,15 @@ import { NextPage } from "next";
 import Head from "next/head";
 import styled from "styled-components";
 
-const Container = styled.div`
-  font-size: 21px;
-  color: gray;
-`;
-
-const index: React.FC = () => {
-  return <div>hello world</div>
+const index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next AirBnB</title>
+      </Head>
+      <Home />
+    </>
+  )
 };
 
 export default index;
