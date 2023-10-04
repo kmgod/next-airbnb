78fd8689e2ed2f4acee96398904417b0569a92d3
androol <multiad05@gmail.com>
2023-10-04 16:16:20 +0900

    /types/api/auth.d.ts added

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,9 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Data from "../../../lib/data";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  //* 계정 생성하기
   if (req.method === 'POST') {
+    //* 값을 받았는지 확인
+
     return res.end();
   }
+  const userExit = Data.user.exist({ email });
+  if (userExit) {
+    res.statusCode = 409;
+    res.send("이미 가입된 이메일입니다.")
+  }
+
   res.statusCode = 405;
 
   return res.end();
---
+++ types/api/auth.d.ts
@@ -0,0 +1,13 @@
+//* 회원가입 Body
+export type SignUpAPIBody = {
+  email: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  birthday: Date;
+}
+//* 로그인 Body
+export type LoginAPIBody {
+  email: string;
+  password: string;
+}
