e81a33e54ed4d543cec65c8129cc84af9e823fb3
androol <multiad05@gmail.com>
2023-10-05 09:33:20 +0900

    DatePicker added

---
+++ components/common/DatePicker.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import ko from 'date-fns/locale/ko';
+import addHours from 'date-fns/addHours';
+import palette from '../../styles/palette';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  .react-datepicker {
+    padding: 16px 32px;
+    background-color: white;
+    box-shadow: rgba(0,0,0,0.2) 0px 6px 20px !important;
+    border-radius: 32px;
+    cursor: default;
+  }
+  .react-datepicker__month-container {
+    padding: 0 27px;
+  }
+  .react-datepicker__header {
+    padding-top: 22px;
+    font-size: 16px;
+    font-weight: 600;
+    border: 0;
+    background-color: white;
+  }
+  .reack-datepicker__navigation--previous {
+    top: 40px;
+    left: 56px;
+    border: 0;
+    background-image: usr("/static/svg/datePicker_left_arrow.svg");
+    background-repeat: no_repeat;
+  }
+  .reack-datepicker__navigation--next {
+    top: 40px;
+    left: 56px;
+    border: 0;
+    background-image: usr("/static/svg/datePicker_right_arrow.svg");
+    background-repeat: no_repeat;
+  }
+  .react-datepicker__current-month {
+    font-size: 16px;
+    font-weight: 600;
+    font-family: Airbnb Cereal, sans-serif;
+  }
+  .react-datepicker__day-names {
+    padding-top: 16px;
+  }
+  .react-datepicker__day-name {
+    width: 48px;
+    margin: 0;
+    font-size: 12px;
+    line-height: 16px;
+    font-weight: 400;
+    color: ${palette.gray_71};
+  }
+  .react-datepicker__month {
+    margin: 0;
+  }
+  .react-datepicker__day {
+    width: 48px;
+    height: 48px;
+    display: inline-fles;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    font-size: 14px;
+    font-weight: 600;
+    font-family: -apple-system, sans-serif;
+    color: ${palette.black};
+    outline: none;
+    &:hover {
+      border: 1px solid ${palette.black};
+      border-radius: 50%;
+    }
+  }
+  .react-datepicker__day--in-range {
+    background-color: ${palette.gray_f7};
+  }
+  .react-datepicker__day--in-selecting-range {
+    background-color: ${palette.gray_f7};
+  }
+  .react-datepicker__day--selected {
+    background-color: ${palette.black};
+    color: white;
+    border-radius: 50%;
+  }
+  .react-datepicker__day--range-start {
+    background-color: ${palette.black};
+    color: white;
+    border-radius: 50%;
+  }
+  .react-datepicker__day--range-end {
+    background-color: ${palette.black};
+    color: white;
+    border-radius: 50%;
+  }
+  .react-datepicker__day--diabled {
+    color: ${palette.gray_dd};
+    cursor: no-drop;
+    &:hover {
+      border: 0;
+    }
+  }
+`;
+
+const DatePicker: React.FC<ReactDatePickerProps> = ({ onChange, ...props }) => {
+  return (
+    <Container>
+      <ReactDatePicker
+        {...props}
+        locale={ko}
+        dateFormat="MM월 dd일"
+        onChange={(date,event) => {
+          if (date) {
+            onChange(addHours(date as Date, 9), event);
+          } else {
+            onChange(null,event)
+          }
+        }}
+        />
+    </Container>
+  );
+};
+export default DatePicker;
--- components/home/SearchRoomBar/SearchRoomCheckInDate.tsx
+++ components/home/SearchRoomBar/SearchRoomCheckInDate.tsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
 import useSearchRoom from '../../../hooks/useSearchRoom';
+import palette from '../../../styles/palette';
+import DatePicker from '../../common/DatePicker';
